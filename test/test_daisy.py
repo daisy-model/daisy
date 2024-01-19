69fbccbae03615cc400e10ed4887c7323550428b
Silas <silas@di.ku.dk>
2024-01-19 15:05:39 +0100

    Four new test cases + rewrite test_daisy.py
    Tests: sample_2.dai test_minimize.dai, test_refman.day test_scn.dai
    
    Cleaner comparison of log files, so it is done in the same way as
    checkpoint files and dlf files

--- test/test_daisy.py
+++ test/test_daisy.py
@@ -36,15 +36,16 @@ def main():
             if entry.is_file():
                 new_file_path = os.path.join(tmpdir, entry.name)
                 if not os.path.exists(new_file_path):
+                    print(f'{new_file_path} does not exist')
                     errors.append(entry.name)
                 else:
                     match = False
                     error_file_path = os.path.join(args.out_dir, entry.name)
-                    if entry.name == 'daisy.log':
+                    if entry.name[-4:] == '.log':
                         match = compare_log_files(entry.path, new_file_path)
                     elif entry.name[-4:] == '.dlf':
                         match = compare_dlf_files(entry.path, new_file_path)
-                    elif entry.name[:10] == 'checkpoint':
+                    elif entry.name[-4:] == '.dai':
                         match = compare_checkpoints(entry.path, new_file_path)
                     else:
                         errors.append(entry.name)                        
@@ -53,20 +54,19 @@ def main():
                         shutil.copy(new_file_path, error_file_path)
 
     if len(errors) > 0:
-        print('Error comparing\n\t', '\n\t'.join(errors))
+        print('== Errors ==', *errors, sep='\n')
         return 2
 
     if len(mismatch) > 0:
-        print('Mismatches\n\t', '\n\t'.join(mismatch))
+        print('== Mismatches ==', *mismatch, sep='\n')
         return 4
 
     return 0
 
 
-
-def keep_line(ignore_tokens):
+def keep_line(ignore_tokens, strip_tokens=None):
     def func(s):
-        s = s.strip()
+        s = s.strip(strip_tokens)
         for tok in ignore_tokens:
             if s.startswith(tok):
                 return False
@@ -80,7 +80,9 @@ def compare_checkpoints(path1, path2):
         lines2 = filter(keep, f2)
         for l1, l2 in zip_longest(lines1, lines2):
             if l1 != l2:
-                print(l1, l2)
+                print('== Checkpoint ==')
+                print(l1.strip())
+                print(l2.strip())
                 return False
         return True
 
@@ -95,61 +97,19 @@ def compare_dlf_files(path1, path2):
                 return False
         return True
 
+
 def compare_log_files(path1, path2):
-    '''Compare two daisy log files
-
-    Everything until the first "* column" line is ignored.
-    Lines starting with 'Time' or 'Program' are ignored
-
-    It is assumed that the files exists
-
-    path1, path2 : str
-      Paths to files to compare
-
-    Returns
-    -------
-    True if files match otherwise False
-    
-    '''
-    start_tokens = [
-        '* column',
-        '* rootdens'
-    ]
-    with open(path1, encoding='utf8') as f1, open(path2, encoding='utf8') as f2:
-        lines1 = list(f1)
-        lines2 = list(f2)
-
-    done = False
-    for i in range(len(lines1)): #pylint: disable=consider-using-enumerate
-        for token in start_tokens:
-            if lines1[i].startswith(token):
-                lines1 = lines1[i:]
-                done = True
-                break
-        if done:
-            break
-        
-    done = False
-    for i in range(len(lines2)): #pylint: disable=consider-using-enumerate
-        for token in start_tokens:
-            if lines2[i].startswith(token):
-                lines2 = lines2[i:]
-                done = True
-                break
-        if done:
-            break
-
-    if len(lines1) != len(lines2):
-        print(len(lines1), len(lines2))        
-        return False
-
-    for l1,l2 in zip(lines1, lines2):
-        if l1 != l2:
-            if not ((l1.startswith('Time') and l2.startswith('Time')) or
-                    (l1.startswith('Program') and l2.startswith('Program'))):
-                print('Lines do not match:', l1, l2)
+    ignore_tokens = ['*', 'Changing', 'Command', 'In', 'Looking', 'Opening', 'Program', 'Reseting',
+                     'Storing', 'Time', 'Trying', 'Using']
+    keep = keep_line(ignore_tokens)
+    with open(path1) as f1, open(path2) as f2:
+        lines1 = filter(keep, f1)
+        lines2 = filter(keep, f2)    
+        for l1, l2 in zip_longest(lines1, lines2):
+            if l1 != l2:
+                print(l1, l2)
                 return False
-    return True
+        return True
 
 if __name__ == '__main__':
     sys.exit(main())
