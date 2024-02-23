9085b22cc79e1ff23502dd1a0818cc26e74f1736
Silas <silas@di.ku.dk>
2024-02-23 14:34:21 +0100

    Reorganize how Daisy is built
    * Move OS specific build stuff to cmake/*
    * Make separate macos and linux presets
    * On linux and mac build daisy as a single binary instead of a source
      library that is linked into the binary

---
+++ cmake/MinGW.cmake
@@ -0,0 +1,33 @@
+# On Windows we build everything except main as a shared library (core)
+# This means that we need to distribute the relevant shared libraries on windows
+add_library(core SHARED)
+target_link_libraries(core PUBLIC
+  cxsparse
+  Boost::filesystem
+)
+target_link_options(core PRIVATE ${LINKER_OPTIONS})
+add_library(windows SHARED)
+add_subdirectory(src/windows)
+target_link_libraries(core PUBLIC
+  windows
+  ${WIN_LIBS}
+)
+
+# On Windows we need to install the dlls that we build and we need to
+# install the dlls we depend on.
+install(TARGETS core RUNTIME DESTINATION bin)
+install(TARGETS windows RUNTIME DESTINATION bin)
+install(FILES
+  $ENV{MINGW_PREFIX}/bin/libstdc++-6.dll
+  $ENV{MINGW_PREFIX}/bin/libwinpthread-1.dll
+  $ENV{MINGW_PREFIX}/bin/libgcc_s_seh-1.dll
+  $ENV{MINGW_PREFIX}/bin/libcxsparse.dll
+  $ENV{MINGW_PREFIX}/bin/libboost_filesystem-mt.dll
+  TYPE BIN
+)
+
+set(DAISY_SAMPLE_DESTINATION "sample")
+set(DAISY_LIB_DESTINATION "lib")
+	"$<IF:$<STREQUAL:${OS},mingw>,lib,${CMAKE_INSTALL_DATADIR}/daisy/lib>"
+)
+set(DAISY_CORE_NAME core)
