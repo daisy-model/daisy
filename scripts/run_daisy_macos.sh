d1b09614c6cb6b2a00500ce3b55c85893a60dff6
Silas Ørting <silas.orting@pm.me>
2025-12-15 13:00:57 +0100

    Print sample location when executed with --info

--- scripts/run_daisy_macos.sh
+++ scripts/run_daisy_macos.sh
@@ -2,6 +2,7 @@
 # Run Daisy with PYTHONHOME and DAISYHOME set
 reldir=$(dirname -- "$0")
 absdir=$(cd -- "$reldir" && pwd)
+sampledir=$(cd -- "$reldir/../sample" && pwd)
 daisy="$absdir/daisy-bin"
 pythonhome="$absdir/../python"
 
@@ -12,9 +13,10 @@ else
     daisyhome="$absdir/.."
 fi
 
-if [[ "$1" == "--version" ]]; then
+if [[ "$1" == "--info" ]]; then
     PYTHONHOME="$pythonhome" "$daisy" -v 2>&1 | head -n 1 && rm daisy.log
     "$absdir"/python --version
+    echo "Sample dir: $sampledir"
 else
     PYTHONHOME="$pythonhome" DAISYHOME="$daisyhome" "$daisy" "$@"
 fi
--- scripts/run_daisy_macos_no_python.sh
+++ scripts/run_daisy_macos_no_python.sh
@@ -2,6 +2,7 @@
 # Run Daisy with DAISYHOME set
 reldir=$(dirname -- "$0")
 absdir=$(cd -- "$reldir" && pwd)
+sampledir=$(cd -- "$reldir/../sample" && pwd)
 daisy="$absdir/daisy-bin"
 
 # Check if user already set DAISYHOME, otherwise set it relative to the script
@@ -11,9 +12,10 @@ else
     daisyhome="$absdir/.."
 fi
 
-if [[ "$1" == "--version" ]]; then
+if [[ "$1" == "--info" ]]; then
     "$daisy" -v 2>&1 | head -n 1 && rm daisy.log
     echo "No python"
+    echo "Sample dir: $sampledir"
 else
     DAISYHOME="$daisyhome" "$daisy" "$@"
 fi
